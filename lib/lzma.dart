d7ccfe191e1d2a3fdae82f3391de5cb0b831fdaf
Robert Ancell <robert.ancell@canonical.com>
2021-07-21 14:14:17 +1200

    Fix copy-paste error in LzmaDecoder class description.
    Capitalize LZMA.

--- lib/lzma.dart
+++ lib/lzma.dart
@@ -5,7 +5,7 @@ import 'src/lzma.dart' as lzma_impl;
 /// Default instance of the [LzmaCodec]
 const LzmaCodec lzma = LzmaCodec();
 
-/// [LzmaCodec] encodes and decodes provided data using the lzma algorithm
+/// [LzmaCodec] encodes and decodes provided data using the LZMA algorithm
 class LzmaCodec extends Codec<List<int>, List<int>> {
   const LzmaCodec();
 
@@ -16,7 +16,7 @@ class LzmaCodec extends Codec<List<int>, List<int>> {
   Converter<List<int>, List<int>> get decoder => const LzmaDecoder();
 }
 
-/// Encoder used to convert the given bytes by applying the lzma compression algorithm
+/// Encoder used to convert the given bytes by applying the LZMA compression algorithm
 class LzmaEncoder extends Converter<List<int>, List<int>> {
   const LzmaEncoder();
 
@@ -29,8 +29,8 @@ class LzmaEncoder extends Converter<List<int>, List<int>> {
   }
 }
 
-/// Encoder used to convert the given bytes by applying the lzma decompression algorithm
-/// The decoder will throw if the provided input does not represent an lzma compressed stream
+/// Decoder used to convert the given bytes by applying the LZMA decompression algorithm
+/// The decoder will throw if the provided input does not represent an LZMA compressed stream
 class LzmaDecoder extends Converter<List<int>, List<int>> {
   const LzmaDecoder();
 
